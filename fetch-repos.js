add7dde9a97bbae17e3806559d15cb8ca6c92385
yusuf eroğlu <78690500+erogluyusuf@users.noreply.github.com>
2025-04-15 17:03:54 +0300

    Update fetch-repos.js

--- fetch-repos.js
+++ fetch-repos.js
@@ -39,11 +39,17 @@ const list = repos
   .slice(0, maxRepos)
   .map(
     (repo) => `
-<table style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 10px;">
-  <tr style="width:65px; padding: 20px; border: 1px solid #ccc; text-align: center;">
-    <td><a href="${repo.html_url}"><img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}&theme=radical" /></a></td>
+<table style="width: 100%; display: flex; flex-wrap: wrap; gap: 10px;">
+  <tr style="display: flex; width: calc(50% - 10px); padding: 20px; border: 1px solid #ccc; text-align: center;">
+    <td style="width: 100%; padding: 10px;">
+      <a href="${repo.html_url}">
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}&theme=radical" />
+      </a>
+    </td>
   </tr>
+  <!-- Diğer satırlar burada olacak -->
 </table>
+
 
 `
   )
