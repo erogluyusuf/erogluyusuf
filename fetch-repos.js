4a2deb0630ab96ed30cc513c59ed58ffae400534
yusuf eroğlu <78690500+erogluyusuf@users.noreply.github.com>
2025-04-15 16:39:11 +0300

    Update fetch-repos.js

--- fetch-repos.js
+++ fetch-repos.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const https = require("https");
 
 const username = "erogluyusuf"; // GitHub kullanıcı adın
-const maxRepos = 5; // En fazla kaç proje gösterilsin
+const maxRepos = 6; // En fazla kaç proje gösterilsin
 
 function fetchRepos(callback) {
   const options = {
@@ -24,18 +24,50 @@ function fetchRepos(callback) {
 
 fetchRepos((repos) => {
   const list = `
-<table>
-  <tr>
+  <style>
+    .repo-container {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      gap: 20px;
+      padding: 20px;
+    }
+    .repo-card {
+      border: 2px solid #f2f2f2;
+      border-radius: 10px;
+      overflow: hidden;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      background-color: #fff;
+      transition: transform 0.2s ease-in-out;
+    }
+    .repo-card:hover {
+      transform: translateY(-10px);
+    }
+    .repo-card img {
+      width: 100%;
+      border-bottom: 2px solid #f2f2f2;
+    }
+    .repo-card a {
+      display: block;
+      padding: 10px;
+      text-align: center;
+      font-size: 14px;
+      font-weight: bold;
+      color: #333;
+      text-decoration: none;
+    }
+  </style>
+
+  <div class="repo-container">
     ${repos.slice(0, maxRepos).map(repo => `
-      <td>
+      <div class="repo-card">
         <a href="${repo.html_url}">
           <img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}&theme=radical" />
+          ${repo.name}
         </a>
-      </td>
+      </div>
     `).join('')}
-  </tr>
-</table>
-`;
+  </div>
+  `;
 
   const readme = fs.readFileSync("README.md", "utf-8");
   const updated = readme.replace(
