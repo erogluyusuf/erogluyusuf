b0f048fcdeea7a884183425cb8d357b1eab51f5e
yusuf eroğlu <78690500+erogluyusuf@users.noreply.github.com>
2025-04-15 16:42:33 +0300

    Update fetch-repos.js

--- fetch-repos.js
+++ fetch-repos.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const https = require("https");
 
 const username = "erogluyusuf"; // GitHub kullanıcı adın
-const maxRepos = 5; // En fazla kaç proje gösterilsin
+const maxRepos = 6; // En fazla kaç proje gösterilsin
 
 function fetchRepos(callback) {
   const options = {
@@ -24,44 +24,11 @@ function fetchRepos(callback) {
 
 fetchRepos((repos) => {
   const list = `
-  <style>
-    .repo-container {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-      gap: 20px;
-      padding: 20px;
-    }
-    .repo-card {
-      border: 2px solid #f2f2f2;
-      border-radius: 10px;
-      overflow: hidden;
-      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-      background-color: #fff;
-      transition: transform 0.2s ease-in-out;
-    }
-    .repo-card:hover {
-      transform: translateY(-10px);
-    }
-    .repo-card img {
-      width: 100%;
-      border-bottom: 2px solid #f2f2f2;
-    }
-    .repo-card a {
-      display: block;
-      padding: 10px;
-      text-align: center;
-      font-size: 14px;
-      font-weight: bold;
-      color: #333;
-      text-decoration: none;
-    }
-  </style>
-
-  <div class="repo-container">
+  <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); gap: 20px; padding: 20px;">
     ${repos.slice(0, maxRepos).map(repo => `
-      <div class="repo-card">
-        <a href="${repo.html_url}">
-          <img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}&theme=radical" />
+      <div style="border: 2px solid #f2f2f2; border-radius: 10px; overflow: hidden; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); background-color: #fff; transition: transform 0.2s ease-in-out;">
+        <a href="${repo.html_url}" style="display: block; padding: 10px; text-align: center; font-size: 14px; font-weight: bold; color: #333; text-decoration: none;">
+          <img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}&theme=radical" style="width: 100%; border-bottom: 2px solid #f2f2f2;" />
           ${repo.name}
         </a>
       </div>
